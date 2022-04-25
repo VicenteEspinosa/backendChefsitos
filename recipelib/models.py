03e2188f92eaab781e8773c5f03291fe8f792169
Vicente Espinosa <vnespinosa@uc.cl>
2022-04-24 20:19:20 -0400

    feat(models): created User model

--- recipelib/models.py
+++ recipelib/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
-class Musician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=100)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500, blank=True)
+    picture_url = models.URLField(blank=True)
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
