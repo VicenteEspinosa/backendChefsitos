d54470a680ad22abca501b0e044d5ce7ece21700
Cristian Hinostroza <cahinostroza@uc.cl>
2022-05-06 02:24:42 -0400

    fix: use filter instead get in user.objects in signup operation and change 201 codes for 200 codes

--- recipelib/operations/users/signin.py
+++ recipelib/operations/users/signin.py
@@ -1,32 +1,49 @@
 import json
+
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
+
 from recipelib.serializers import UserSerializer
 
 schema = {
-    'type':'object',
-    'properties': {
-        'username': {
-            'type':'string'
-        },
-        'password': {
-            'type': 'string'
-        }
+    "type": "object",
+    "properties": {
+        "username": {"type": "string"},
+        "password": {"type": "string"},
     },
-    'required': ['username', 'password']
+    "required": ["username", "password"],
 }
 
+
 def signin(req, data):
-  try:
-    user = authenticate(req, username=data.get('username'), password=data.get('password'))
-    if user is not None:
-      login(req, user)
-      return JsonResponse(UserSerializer(user).data, safe=False, status=201)
-    else:
-      return JsonResponse({"internalCode":"entity-not-found", "message": "'user' is not found"}, safe=False, status=404)
-  except Exception as err:
-    print(err)
-    return JsonResponse({"internalCode":"internal-error", "message":"An error has ocurred"}, safe=False, status=500)
+    try:
+        user = authenticate(
+            req, username=data.get("username"), password=data.get("password")
+        )
+        if user is not None:
+            login(req, user)
+            return JsonResponse(
+                UserSerializer(user).data, safe=False, status=200
+            )
+        else:
+            return JsonResponse(
+                {
+                    "internalCode": "entity-not-found",
+                    "message": "'user' is not found",
+                },
+                safe=False,
+                status=404,
+            )
+    except Exception as err:
+        print(err)
+        return JsonResponse(
+            {
+                "internalCode": "internal-error",
+                "message": "An error has ocurred",
+            },
+            safe=False,
+            status=500,
+        )
+
 
 signin.schema = schema
-    
--- recipelib/operations/users/signout.py
+++ recipelib/operations/users/signout.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import logout
 from django.http import JsonResponse
 
+
 def signout(request):
-  try:
-    logout(request)
-    return JsonResponse({}, safe=False, status=201)
-  except Exception as err:
-    print(err)
-    return JsonResponse({"internalCode":"internal-error", "message":"An error has ocurred"}, safe=False, status=500)
-  
+    try:
+        logout(request)
+        return JsonResponse({}, safe=False, status=200)
+    except Exception as err:
+        print(err)
+        return JsonResponse(
+            {
+                "internalCode": "internal-error",
+                "message": "An error has ocurred",
+            },
+            safe=False,
+            status=500,
+        )
