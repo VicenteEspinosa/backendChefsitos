40b554608862ba3deab536a9e7074fb378918abe
tfflores <tfflores@uc.cl>
2022-05-17 02:47:25 -0400

    test: added unit tests for measurement endpoints

--- recipelib/urls.py
+++ recipelib/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
   path('measurements/', views.MeasurementView.as_view(), name='measurements'),
-  path('measurements/<int:measurement_id>', views.MeasurementView.as_view(), name='measurements'),
-  path('users/signup', views.UserSignup.as_view(), name='user_signup'),
-  path('users/signin', views.UserSignin.as_view(), name='user_signin'),
-  path('users/signout', views.UserSignout.as_view(), name='user_signout')
+  path('measurements/<int:measurement_id>/', views.MeasurementView.as_view(), name='measurements'),
+  path('users/signup/', views.UserSignup.as_view(), name='user_signup'),
+  path('users/signin/', views.UserSignin.as_view(), name='user_signin'),
+  path('users/signout/', views.UserSignout.as_view(), name='user_signout')
 ]
