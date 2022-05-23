0fc907d3a6a049f78391754b6b7f74c6e3012a25
Vicente Espinosa <vnespinosa@uc.cl>
2022-05-23 11:18:48 -0400

    feat: created sefl_recipes endpoint

--- recipelib/operations/recipes/recipe_actions.py
+++ recipelib/operations/recipes/recipe_actions.py
@@ -58,3 +58,16 @@ def create_recipe(req, data):
 
 
 create_recipe.schema = schema
+
+
+def get_self_recipes(req):
+    try:
+        recipes = Recipe.objects.filter(user=req.user)
+        return JsonResponse(
+            RecipeSerializer(recipes, many=True).data,
+            safe=False,
+            status=200,
+        )
+    except Exception as err:
+        print(err)
+        return error_json_response(err)
--- recipelib/views.py
+++ recipelib/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from recipelib.api_views.ingredient import IngredientView
 from recipelib.api_views.measurement import MeasurementView
-from recipelib.api_views.recipe import RecipeView
+from recipelib.api_views.recipe import RecipeView, SelfRecipesView
 from recipelib.api_views.user import UserSignin, UserSignout, UserSignup
