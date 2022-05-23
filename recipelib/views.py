fb128f116ca6946369b59f2c0d55a5d6904441dc
AbyssalBit <hrhartmann@uc.cl>
2022-05-23 13:10:03 -0400

    merge conflict

--- recipelib/views.py
+++ recipelib/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-from recipelib.api_views.ingredient import IngredientView
+# from recipelib.api_views.ingredient import IngredientView
 from recipelib.api_views.measurement import MeasurementView
-from recipelib.api_views.recipe import RecipeView
+
+# from recipelib.api_views.recipe import RecipeView
 from recipelib.api_views.user import (
     UserSignin,
     UserSignout,
