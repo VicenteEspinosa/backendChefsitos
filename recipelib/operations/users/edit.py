df602708371e090c78a1acfce233075ffb485553
AbyssalBit <hrhartmann@uc.cl>
2022-05-23 13:08:04 -0400

    fix(edit): not requested and other minor changes

--- recipelib/operations/users/edit.py
+++ recipelib/operations/users/edit.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 from recipelib.infrastructure.validation.formats import email
 from recipelib.models import User
 from recipelib.serializers import UserSerializer
+from recipelib.utils import already_exists_json_response, internal_error
 
 schema = {
     "type": "object",
@@ -13,47 +14,44 @@ schema = {
         "email": {"pattern": email, "type": "string"},
         "first_name": {"type": "string"},
         "last_name": {"type": "string"},
+        "description": {"type": "string"},
+        "picture_url": {"type": "string"},
+        "password": {"type": "string"},
     },
-    "required": ["username", "email", "first_name", "last_name"],
+    "required": [],
 }
 
 
 def edit(request, data):
     try:
         any = User.objects.filter(username=data.get("username"))
-        print(any)
-        print(data.get("username"))
         if not any:
             old_username = request.user.username
             user = User.objects.filter(username=old_username)[0]
-            user.username = data.get("username")
-            user.email = data.get("email")
-            user.first_name = data.get("first_name")
-            user.last_name = data.get("last_name")
+            if data.get("username"):
+                user.username = data.get("username")
+            if data.get("email"):
+                user.email = data.get("email")
+            if data.get("first_name"):
+                user.first_name = data.get("first_name")
+            if data.get("last_name"):
+                user.last_name = data.get("last_name")
+            if data.get("description"):
+                user.profile.description = data.get("description")
+            if data.get("picture_url"):
+                user.profile.picture_url = data.get("picture_url")
+            if data.get("password"):
+                user.set_password(data.get("password"))
             user.save()
             return JsonResponse(
                 UserSerializer(user).data, safe=False, status=201
             )
-        return JsonResponse(
-            {
-                "internalCode": "entity-not-processable",
-                "path": "username",
-                "message": "'username' already exists",
-            },
-            safe=False,
-            status=422,
+        return already_exists_json_response(
+            "User", "Username", data.get("username")
         )
     except Exception as err:
         print(err)
-        return JsonResponse(
-            {
-                "internalCode": "internal-error",
-                "message": "An error has ocurred",
-                "message": "An error has ocurred",
-            },
-            safe=False,
-            status=500,
-        )
+        return internal_error
 
 
 edit.schema = schema
