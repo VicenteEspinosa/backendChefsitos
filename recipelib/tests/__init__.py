2ce5d4c83140a2c528dd84c36ec1ac0ad8d6b057
tfflores <tfflores@uc.cl>
2022-05-25 01:20:36 -0400

    test: added auth tests

--- recipelib/tests/__init__.py
+++ recipelib/tests/__init__.py
@@ -1,3 +1,4 @@
+from .test_auth.test_views import TestAuthViews
 from .test_ingredients.test_views import TestIngredientViews
 from .test_measurements.test_views import TestMeasurementViews
 from .test_recipes.test_views import TestRecipeViews
