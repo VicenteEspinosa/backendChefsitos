2927478b8a5d4d7f3b660dc11aeda2d3fb3539fa
Cristian Hinostroza <cahinostroza@uc.cl>
2022-06-10 13:26:30 -0400

    feat: create tests for feed

--- recipelib/tests/test_recipes/test_views.py
+++ recipelib/tests/test_recipes/test_views.py
@@ -12,6 +12,7 @@ from recipelib.models import (
     Tag,
     User,
 )
+from recipelib.serializers import RecipeSerializer
 
 
 class TestRecipeViews(TransactionTestCase):
@@ -143,3 +144,48 @@ class TestRecipeViews(TransactionTestCase):
         url = reverse("recipes", args=[1])
         response = client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_GET_feed(self):
+        client = Client()
+        client.force_login(
+            User.objects.get_or_create(username="generic_user")[0]
+        )
+        url = reverse("feed")
+        response = client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(data, list)
+
+    def test_GET_feed_other_users_recipe_is_included(self):
+        client = Client()
+        client.force_login(
+            User.objects.get_or_create(username="generic_user")[0]
+        )
+        recipe = Recipe.objects.create(
+            user=User.objects.get_or_create(username="other_generic_user")[0],
+            name="cebolla picada",
+            description="",
+            private=False,
+            picture_url="",
+        )
+        url = reverse("feed")
+        response = client.get(url)
+        data = response.json()
+        self.assertEqual(RecipeSerializer(recipe).data in data, True)
+
+    def test_GET_feed_req_users_recipe_is_not_included(self):
+        client = Client()
+        client.force_login(
+            User.objects.get_or_create(username="generic_user")[0]
+        )
+        recipe = Recipe.objects.create(
+            user=User.objects.get_or_create(username="generic_user")[0],
+            name="cebolla picada",
+            description="",
+            private=False,
+            picture_url="",
+        )
+        url = reverse("feed")
+        response = client.get(url)
+        data = response.json()
+        self.assertEqual(RecipeSerializer(recipe).data in data, False)
